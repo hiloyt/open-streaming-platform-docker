804252671bca329f6506e230a3002ae14a7478c0
Deamos <dave.r.lockwood@gmail.com>
2021-01-25 21:12:37 -0500

    Adding XMLRPC Authorized IPs

--- 0.8.0/Ejabberd/docker-files.d/entrypoint.sh
+++ 0.8.0/Ejabberd/docker-files.d/entrypoint.sh
@@ -6,11 +6,21 @@ cp -u -p /home/ejabberd/run/auth_osp.py /home/ejabberd/conf/auth_osp.py
 export EJABBERD_DOMAIN
 sed -i "s/CHANGEME/$EJABBERD_DOMAIN/g" /home/ejabberd/conf/ejabberd.yml
 
+export EJABBERD_XMLRPC_ALLOWIP
+IFS="," read -a XMLRPCARRAY <<< $EJABBERD_XMLRPC_ALLOWIP
+XMLRPCSTRING=""
+for i in "${XMLRPCARRAY[@]}"
+do
+      XMLRPCSTRING+="      - $i\n"
+done
+sed -i "s/#ALLOWXMLRPC/$XMLRPCSTRING/g" /home/ejabberd/conf/ejabberd.yml
+
 export OSP_API_PROTOCOL
 export OSP_API_DOMAIN
-export EJABBERD_PASSWORD
 
 sed -i "s/protocol = \"http\"/protocol=\"$OSP_API_PROTOCOL\"/g" /home/ejabberd/conf/auth_osp.py
 sed -i "s/ospAPIServer = \"127.0.0.1:5010\"/ospAPIServer=\"$OSP_API_DOMAIN\"/g" /home/ejabberd/conf/auth_osp.py
+
+export EJABBERD_PASSWORD
 
 /home/ejabberd/bin/ejabberdctl foreground
