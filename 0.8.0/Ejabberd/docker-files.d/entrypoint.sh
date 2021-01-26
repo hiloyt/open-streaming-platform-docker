a6f40cad9d2a3026b7f2e35c9c620ad028877e8e
Deamos <dave.r.lockwood@gmail.com>
2021-01-25 22:03:56 -0500

    Fixes

--- 0.8.0/Ejabberd/docker-files.d/entrypoint.sh
+++ 0.8.0/Ejabberd/docker-files.d/entrypoint.sh
@@ -6,14 +6,14 @@ cp -u -p /home/ejabberd/run/auth_osp.py /home/ejabberd/conf/auth_osp.py
 export EJABBERD_DOMAIN
 sed -i "s/CHANGEME/$EJABBERD_DOMAIN/g" /home/ejabberd/conf/ejabberd.yml
 
-export EJABBERD_XMLRPC_ALLOWIP
-IFS="," read -a XMLRPCARRAY <<< $EJABBERD_XMLRPC_ALLOWIP
-XMLRPCSTRING=""
-for i in "${XMLRPCARRAY[@]}"
-do
-      XMLRPCSTRING+="      - $i\n"
-done
-sed -i "s/#ALLOWXMLRPC/$XMLRPCSTRING/g" /home/ejabberd/conf/ejabberd.yml
+#export EJABBERD_XMLRPC_ALLOWIP
+#IFS="," read -a XMLRPCARRAY <<< $EJABBERD_XMLRPC_ALLOWIP
+#XMLRPCSTRING=""
+#for i in "${XMLRPCARRAY[@]}"
+#do
+#      XMLRPCSTRING+="      - $i\n"
+#done
+#sed -i "s/ALLOWXMLRPC/$XMLRPCSTRING/g" /home/ejabberd/conf/ejabberd.yml
 
 export OSP_API_PROTOCOL
 export OSP_API_DOMAIN
@@ -23,4 +23,5 @@ sed -i "s/ospAPIServer = \"127.0.0.1:5010\"/ospAPIServer=\"$OSP_API_DOMAIN\"/g" 
 
 export EJABBERD_PASSWORD
 
+/home/ejabberd/bin/ejabberdctl register admin localhost $EJABBERD_PASSWORD
 /home/ejabberd/bin/ejabberdctl foreground
