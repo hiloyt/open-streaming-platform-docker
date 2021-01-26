21ff52f4b172dc724a8e8c233d71e3ab4d02de85
Deamos <dave.r.lockwood@gmail.com>
2021-01-25 19:58:10 -0500

    Fixes x 9999

--- 0.8.0/Ejabberd/docker-files.d/entrypoint.sh
+++ 0.8.0/Ejabberd/docker-files.d/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo cp -u -p /home/ejabberd/run/ejabberd.yml /home/ejabberd/conf/ejabberd.yml
-sudo cp -u -p /home/ejabberd/run/auth_osp.py /home/ejabberd/conf/auth_osp.py
+cp -u -p /home/ejabberd/run/ejabberd.yml /home/ejabberd/conf/ejabberd.yml
+cp -u -p /home/ejabberd/run/auth_osp.py /home/ejabberd/conf/auth_osp.py
 
 # Configure ejabberd
 export EJABBERD_DOMAIN
@@ -13,4 +13,4 @@ export EJABBERD_PASSWORD
 sed -i "s/protocol = \"http\"/protocol=\"$OSP_API_PROTOCOL\"/g" /home/ejabberd/conf/auth_osp.py
 sed -i "s/ospAPIServer = \"127.0.0.1:5010\"/ospAPIServer=\"$OSP_API_DOMAIN\"/g" /home/ejabberd/conf/auth_osp.py
 
-/home/ejabberd/bin/ejabberdctl -foreground
+/home/ejabberd/bin/ejabberdctl foreground
