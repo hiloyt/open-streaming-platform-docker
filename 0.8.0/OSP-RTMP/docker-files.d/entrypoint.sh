170c6a7254bffba2fe183fdfe0e436296f4a1ced
Deamos <dave.r.lockwood@gmail.com>
2021-02-06 12:43:45 -0500

    Fixes

--- 0.8.0/OSP-RTMP/docker-files.d/entrypoint.sh
+++ 0.8.0/OSP-RTMP/docker-files.d/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo 'Placing Configuration Files'
 cp -u -p /opt/osp-rtmp/setup/nginx/servers/* /usr/local/nginx/conf/servers
-cp -u -p /opt/osp-rtmp/setup/nginx/services/* /usr/local/nginx/conf/upstream
+cp -u -p /opt/osp-rtmp/setup/nginx/services/* /usr/local/nginx/conf/services
 
 echo 'Setting up Directories'
   mkdir -p /var/www && \
